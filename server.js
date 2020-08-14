7f8c5756c92fc8c8862eb5b595189a0f6f596409
sevendc <benford.mark783@gmail.com>
2020-08-14 14:55:16 +0400

    feat: add basic components

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const rooms = new Map([
+
+]);
+
+app.get("/rooms", (req, res) => {
+
+  res.json(rooms);
+});
+
+io.on('connection', socket => {
+  console.log('user connected', socket.id);
+})
+
+server.listen(3001, (err) => {
+  if (err) {
+    throw Error(err);
+  }
+  console.log("Server is running on: http://localhost:3001");
+});
