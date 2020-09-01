25756fe882ba95c5427b4b49c82abc37dead6cb9
sevendc <benford.mark783@gmail.com>
2020-09-01 18:35:20 +0400

    feat: add reducer

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 const express = require("express");
-
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
-const rooms = new Map([
+app.use(express.json());
 
-]);
+const rooms = new Map();
 
 app.get("/rooms", (req, res) => {
-
   res.json(rooms);
 });
 
-io.on('connection', socket => {
-  console.log('user connected', socket.id);
-})
+app.post("/rooms", (req, res) => {
+  const { roomId, userName } = req.body;
+  if (!rooms.has(roomId)) {
+    rooms.set(roomId, new Map(["users", new Map()], ["messages", []]));
+  }
+  res.send();
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected", socket.id);
+});
 
 server.listen(3001, (err) => {
   if (err) {
--- src/components/JoinBlock.jsx
+++ src/components/JoinBlock.jsx
@@ -1,12 +1,43 @@
-import React from "react";
-import socket from '../socket';
+import React, { useState } from "react";
+import axios from "axios";
+import socket from "../socket";
 
-export default function joinBlock() {
+function JoinBlock({ onLogin }) {
+  const [roomId, setRoomId] = useState("");
+  const [userName, setUserName] = useState("");
+
+  const onEnter = () => {
+    if (!roomId || !userName) {
+      return alert("Некорректные данные");
+    }
+    axios
+      .post("/rooms", {
+        roomId,
+        userName,
+      })
+      .then(onLogin);
+    console.log(roomId, userName);
+  };
+
   return (
     <div className="join-block">
-      <input type="text" placeholder="Room ID"/>
-      <input type="text" placeholder="Ваше имя"/>
-      <button className="btn btn-success">Войти</button>
+      <input
+        type="text"
+        placeholder="Room ID"
+        value={roomId}
+        onChange={(e) => setRoomId(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Ваше имя"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button className="btn btn-success" onClick={onEnter}>
+        Войти
+      </button>
     </div>
   );
 }
+
+export default JoinBlock;
